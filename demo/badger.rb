e02ad517d3be0d8576b70bc767b2912af825c089
Janiss Binder <binder@puzzle.ch>
2022-10-18 10:46:44 +0200

    Update demo rbs class

--- demo/badger.rb
+++ demo/badger.rb
@@ -1,11 +1,22 @@
 class Badger
-  attr_reader :brand
 
-  def initialize(brand = "Honeybadger")
-    @brand = brand
+    attr_reader :brand
+
+    def initialize(brand)
+      @brand = brand
+    end
+
+end
+
+class Honey < Badger
+
+  def initialize(brand: "Honeybadger", sweet: true)
+    super(brand)
+    @sweet = sweet
   end
 
-  def brand?
-    @brand
+  def sweet?
+    @sweet
   end
+
 end
